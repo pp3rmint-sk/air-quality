06e28973587880430ec377aafa43f29d638f46a9
ChloeH-B <chloehb@MacBook-Air-de-Chloe.local>
2020-08-08 12:29:38 +0200

    feat(auth): route correction + added logout btn

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -8,7 +8,8 @@
     <b-collapse id="nav-collapse" is-nav>
       <b-navbar-nav>
         <b-nav-item to="/">Home</b-nav-item>
-        <b-nav-item to="/login">Admin</b-nav-item>
+        <b-nav-item to="/admin">Admin</b-nav-item>
+         <b-nav-item v-if="connected" @click="logout">Logout</b-nav-item>
       </b-navbar-nav>
 
     </b-collapse>
@@ -17,8 +18,32 @@
 </template>
 
 <script>
+import { auth } from "@/firebase";
+
     export default {
         name: "NavBar",
+        data() {
+          return {
+            connected: false,
+          }
+        },
+
+       mounted () {
+         auth.onAuthStateChanged(user=>{
+           if (user) {
+             this.connected = true
+           } else {
+             this.connected = false
+           }
+         });
+       },
+
+       methods: {
+         logout() {
+           auth.signOut();
+           this.$router.replace("/");
+         }
+       },
 
     }
 </script>
